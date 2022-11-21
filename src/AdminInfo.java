d7def0ff90acf5fa15968ca214bbace19fd15fdb
leyton1509 <taylor92@lancaster.ac.uk>
2022-11-21 16:10:10 +0000

    Added admin info class which containts the username and password and getter methods

---
+++ src/AdminInfo.java
@@ -0,0 +1,19 @@
+public class AdminInfo {
+
+    private String username;
+    private String password;
+
+    public AdminInfo(){
+        username = "leyton";
+        password = "abc123";
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+}
--- src/Driver.java
+++ src/Driver.java
@@ -13,6 +13,31 @@ public class Driver {
         String password = myObj.nextLine();
 
 
+        AdminInfo adminInfo = new AdminInfo();
+
+        int tries = 0;
+        boolean success = false;
+
+        while(tries < 3){
+
+            if(userName.equals(adminInfo.getUsername()) && password.equals(adminInfo.getPassword())){
+                System.out.println("Successful logging");
+                success = true;
+                tries = 3;
+            }
+            else{
+                tries++;
+                System.out.println("Failed logging, attempt number" + tries + " try again!");
+            }
+
+        }
+
+        if(success == false){
+            System.out.println("No more attempts to try, logging failed");
+        }
+
+
+
 
     }
 }
